79c0ea6d708a5cc899b05d1f6548b07000f895ab
KoknaevD <89024464050@yandex.ru>
2020-08-25 00:16:30 +0500

    Yay! You've completed Stage 1/4!

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,41 @@
-import java.util.*
+package phonebook
+
+import java.io.File
 
 fun main() {
-    println("hello")
+
+
+    val namesFileName = "C:\\Users\\Dima\\IdeaProjects\\find.txt"
+    val phonesFileName = "C:\\Users\\Dima\\IdeaProjects\\directory.txt"
+    val names = File(namesFileName).readLines()
+    val phones = File(phonesFileName).readLines()
+    println("Start searching...")
+    val startTime = System.currentTimeMillis()
+    var count = 0
+
+    for (line in phones) {
+        val data = line.split(' ')
+        var fname = ""
+
+        if (data.size == 2) {
+            fname = data[1]
+        } else {
+            fname = "${data[1]} ${data[2]}"
+        }
+
+        for (name in names) {
+            if (fname == name) {
+                count++
+                break
+            }
+        }
+    }
+
+    val stopTime = System.currentTimeMillis()
+    val total = stopTime - startTime
+    var sec = (total) / 1000
+    val ms = total - sec * 1000
+    val min = sec / 60
+    sec -= min * 60
+    println("Found $count / ${names.size} entries. Time taken: $min min. $sec sec. $ms ms.")
 }
